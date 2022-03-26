fdf74510b456e421617d42265b196d9cacde6e8a
Karol <karol@4-you.pl>
2022-03-26 16:42:35 +0100

    test

---
+++ cmake-build-default/CMakeFiles/task.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task.dir/pjatkCPP/cpp3/s25417.cpp.obj"
+  "CMakeFiles/task.dir/pjatkCPP/cpp3/s25417.cpp.obj.d"
+  "libtask.dll.a"
+  "task.exe"
+  "task.exe.manifest"
+  "task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
